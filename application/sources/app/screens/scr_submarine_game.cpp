29be4db5078868c008e5a85b1eca73f4e19ea8f8
KhaThai <williamalbus26@gmail.com>
2026-05-25 14:38:17 +0700

    fix commit git for hit submarine

--- application/sources/app/screens/scr_submarine_game.cpp
+++ application/sources/app/screens/scr_submarine_game.cpp
@@ -141,22 +141,15 @@ void scr_submarine_game_handle(ak_msg_t *msg)
             }
         }
 
-        /* Kiểm tra đạn địch trúng tàu player , va cham tau ngam  */
-        if (sub_game_enemy_bullet_hit_submarine() )
+        /* Kiểm tra đạn địch trúng tàu player , */
+        if (sub_game_enemy_bullet_hit_submarine() && sub_game_obstacle_hit_submarine())
         {
             sub_game_bang_spawn(submarine.x, submarine.y);
             BUZZER_PlayTones(tones_3beep);
             game_state = GAME_STATE_OVER;
             timer_remove_attr(AC_TASK_DISPLAY_ID, SB_GAME_TIME_TICK);
         }
-        /* Kiểm tra va chạm tàu ngầm */
-        if (sub_game_obstacle_hit_submarine())
-        {
-            sub_game_bang_spawn(submarine.x, submarine.y);
-            BUZZER_PlayTones(tones_3beep);
-            game_state = GAME_STATE_OVER;
-            timer_remove_attr(AC_TASK_DISPLAY_ID, SB_GAME_TIME_TICK);
-        }
+  
     }
     break;
 
